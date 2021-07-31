23f2f5b3736f5f8a9ba8def02304d6f0075d9add
Theresa Lehr <theresalehr@Theresas-MacBook-Pro.local>
2021-07-31 12:33:20 -0700

    limps along, commiting now in case I break it even more.

--- MagicEightBall/Controllers/HangmanController.cs
+++ MagicEightBall/Controllers/HangmanController.cs
@@ -16,13 +16,19 @@ namespace MagicEightBall.Controllers
         public IActionResult Index()
         {
             return View();
+
+
+        }
+
+        public IActionResult CreateHangMan()
+        {
+            return View();
         }
 
         public IActionResult Hangman(string Input)
         {
             //ViewBag.Input = Input;
             //List<string> answers = new List<string> { "apple" };
-            //ViewBag.answers = answers;
             //List<string> emptySpaces = new List<string> { };
             //ViewBag.emptySpaces = emptySpaces;
             //int roundCap = 6;
@@ -48,12 +54,15 @@ namespace MagicEightBall.Controllers
 
             Hangman newHangman = new Hangman();
             string answer = newHangman.Answer;
+            ViewBag.answers = answer;
+            ViewBag.input = Input;
+
             //ViewBag.roundCap = newHangman.RoundCap;
 
-                for (int i = 0; i < answer.Length; i++)
-                {
-                    HangManData.emptySpaces.Add('_');
-                }
+                //for (int i = 0; i < answer.Length; i++)
+                //{
+                //    HangManData.emptySpaces.Add('_');
+                //}
 
                 if (Input != null)
                 {
@@ -67,25 +76,23 @@ namespace MagicEightBall.Controllers
 
                             else if (answer.Contains(Input) != true)
                             {
-                                HangManData.DecreaseRoundCap(newHangman);
-                                return View();
-                                //}
+                                newHangman.DecreaseRoundCap();
                             }
 
-                            //if (newHangman.RoundCap == 0)
-                            //{
-                            //    "<p>You lost. Because of you, an innocent man has died.</p>";
-                            //}
-                            //else
-                            //{
-                            //    Hangman.Answer;
-                            //    "<p>CONGRATS!! You won!!!</p>
-                            //}
-                        //}
+                if (newHangman.RoundCap == 0)
+                {
+                    return Content("<p class='gameOver'>You lost. Because of you an innocent man has died.</p> <br /> <a asp-controller='Hangman' asp-action='Hangman'>Play again?</a>", "text/html");
+                }
+                else if (!HangManData.emptySpaces.Contains('_'))
+                {
+                    return Content("<p class='gameOver'>CONGRATS!! You won!!!</p> <br /> <button asp-controller='Hangman' asp-action='Hangman'>Play again?</button>", "text/html");
+                }
+                //}
 
-            //while (HangManData.emptySpaces.Contains('_') && (newHangman.RoundCap != 0));
+                //while (HangManData.emptySpaces.Contains('_') && (newHangman.RoundCap != 0));
             }
             
+            //HangManData.SetEmptySpaces(newHangman);
             ViewBag.roundCap = newHangman.RoundCap;
             return View(HangManData.emptySpaces);
             }
--- MagicEightBall/Models/Hangman.cs
+++ MagicEightBall/Models/Hangman.cs
@@ -5,21 +5,31 @@ namespace MagicEightBall.Models
 {
     public class Hangman
     {
-        public string Answer { get; set; }
-        //private static int _roundCap = 6;
-        public int RoundCap { get; set; } = 6;
+        private static Random wordPicker = new Random();
+        private static string _answer = HangManData.answers[wordPicker.Next(HangManData.answers.Count)];
+        public string Answer { get; }
+        private static int _roundCap = 6;
+        public int RoundCap { get; } 
 
         public Hangman()
         {
-            Random wordPicker = new Random();
-            string thisAnswer = HangManData.answers[wordPicker.Next(HangManData.answers.Count)];
-            this.Answer = thisAnswer;
-            //RoundCap = _roundCap;
+            //Random wordPicker = new Random();
+            //string thisAnswer = HangManData.answers[wordPicker.Next(HangManData.answers.Count)];
+            //this.Answer = thisAnswer;
+            RoundCap = _roundCap;
+            Answer = _answer;
+            //HangManData.SetEmptySpaces(Answer.Length);
+
+
+            //for (int i = 0; i < Answer.Length; i++)
+            //{
+            //    HangManData.emptySpaces.Add('_');
+            //}
         }
 
         public void DecreaseRoundCap()
         {
-            RoundCap--;
+            _roundCap--;
         }
     }
 }
