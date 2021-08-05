7a4af2132a25de60e89d280fdd8781051ca33f8e
Theresa Lehr <theresalehr@Theresas-MacBook-Pro.local>
2021-08-05 13:04:25 -0700

    added views

---
+++ MagicEightBall/Data/HangManData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MagicEightBall.Models;
+
+namespace MagicEightBall.Data
+{
+    public class HangManData
+    {
+        public static List<string> answers = new List<string> { "peach", "grape" };
+        public static List<char> emptySpaces = new List<char> { '_', '_', '_', '_', '_' };
+        public static List<Hangman> ourHangman = new List<Hangman> { };
+
+        // = new List<char> {'_', '_', '_', '_', '_'};
+
+        public static void Add(char input, Hangman newHangman)
+        {
+            foreach (char i in newHangman.Answer)
+            {
+                if (i == input)
+                {
+                    int num = newHangman.Answer.IndexOf(input);
+                    emptySpaces[num] = input;
+                }
+            }
+        }
+
+        public static void SetEmptySpaces(int answer)
+        {
+            for (int i = 0; i < answer; i++)
+            {
+
+                emptySpaces.Add('_');
+            }
+        }
+    }
+}
